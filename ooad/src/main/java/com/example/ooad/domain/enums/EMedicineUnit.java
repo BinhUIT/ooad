1860da0e2f54a6b0358f92ba13129f9d202eab6f
leminionn <minhlephan1901@gmail.com>
2025-12-30 17:30:00 +0700

    feat: Implement MedicineController and enhance EMedicineUnit and MedicineRepository for improved medicine management

--- ooad/src/main/java/com/example/ooad/domain/enums/EMedicineUnit.java
+++ ooad/src/main/java/com/example/ooad/domain/enums/EMedicineUnit.java
@@ -2,10 +2,14 @@ package com.example.ooad.domain.enums;
 
 public enum EMedicineUnit {
     TABLET,
+    CAPSULE,
     BLISTER,
     BOX,
     BOTTLE,
+    TUBE,
     VIAL,
     AMPOULE,
-    SACHET
+    SACHET,
+    PACK,
+    BAG
 }
